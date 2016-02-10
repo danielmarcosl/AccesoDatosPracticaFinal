d56358e5e1b09fa95a15cabcd384e7adcc16d562
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-10 21:21:15 +0100

    Update sql

--- PracticaSQLiteHospital/hospital.sql
+++ PracticaSQLiteHospital/hospital.sql
@@ -1,12 +1,13 @@
 create table if not exists habitacion(
-cod_hab smallint(3) primary key,
+cod_hab integer primary key autoincrement,
 planta smallint(2),
-num_cama smallint(3) unique,
-doctorasignado varchar(20)
+num_cama smallint(3),
+doctorasignado varchar(20),
+ocupado char(1)
 );
 
 create table if not exists paciente(
-cod_pac integer primary key,
+cod_pac integer primary key autoincrement,
 dni varchar(10) unique,
 nombre varchar(20),
 edad integer,
@@ -17,9 +18,21 @@ constraint fk_paciente_cod_hab foreign key (cod_hab) references habitacion(cod_h
 );
 
 insert into habitacion values
-(101,1,1,'El barbas'),
-(102,1,2,'Peluquero'),
-(201,2,3,'El tirillas'),
-(202,2,4,'Cigala'),
-(301,3,5,'Pasarlo bien'),
-(302,3,6,'Aprovecharlo');
+(null,1,101,'Papo','N'),
+(null,1,102,'Golondrio','S'),
+(null,1,103,'Papo','N'),
+(null,2,201,'Papo','N'),
+(null,2,202,'Golondrio','S'),
+(null,2,203,'Berer','S'),
+(null,3,301,'Berer','N'),
+(null,3,302,'Buenaesa','S'),
+(null,3,303,'Papo','N'),
+(null,4,401,'Berer','N'),
+(null,4,402,'Golondrio','N'),
+(null,4,403,'Golondrio','N');
+
+insert into paciente values
+(null,'1234A','Martinez',25,'H','Muchas',2),
+(null,'5612C','Birreta',24,'M','Pocas',5),
+(null,'5647E','Siberaso',19,'M','Unas cuantas',6),
+(null,'7598J','sqlito',34,'H','Tres',8);
