a6539353948e5d68862b85342dc5d72bf8e4886e
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2016-02-10 20:39:58 +0100

    Update vista

--- PracticaSQLiteHospital/src/practicasqlitehospital/Vista.java
+++ PracticaSQLiteHospital/src/practicasqlitehospital/Vista.java
@@ -20,6 +20,7 @@ public class Vista extends JFrame {
     static JButton bAlta;
     static JButton bBaja;
     static JButton bMod;
+    static JButton bList;
     static JButton bSalir;
     // Campo de texto
     static JTextField campoTexto;
@@ -31,13 +32,14 @@ public class Vista extends JFrame {
         // Detalles de la ventana
         setTitle("Hospital");
         setLayout(null);
-        setBounds(0, 0, 435, 440);
+        setBounds(0, 0, 525, 440);
         setResizable(false);
 
         // Inicializamos los botones
         bAlta = new JButton("Alta");
         bBaja = new JButton("Baja");
         bMod = new JButton("Modificar");
+        bList = new JButton("Listado");
         bSalir = new JButton("Salir");
 
         // Inicializamos el campod e texto
@@ -54,15 +56,17 @@ public class Vista extends JFrame {
         add(bBaja);
         bMod.setBounds(210, 10, 100, 30);
         add(bMod);
-        bSalir.setBounds(310, 10, 100, 30);
+        bList.setBounds(310, 10, 100, 30);
+        add(bList);
+        bSalir.setBounds(410, 10, 100, 30);
         add(bSalir);
 
         // Posicionamos el scroll y lo anadimos a la ventana
-        scrollpanel.setBounds(10, 50, 400, 300);
+        scrollpanel.setBounds(10, 50, 500, 300);
         add(scrollpanel);
 
         // Posicionamos el campo de texto y lo anadimos a la ventana
-        campoTexto.setBounds(10, 360, 400, 40);
+        campoTexto.setBounds(10, 360, 500, 40);
         add(campoTexto);
 
         // Hacemos que el panel de texto no sea editable
